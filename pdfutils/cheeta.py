f8c2dcddeea0a1848a25ddf0a385add48653aad4
ashoksiri <ashoksiri@yahoo.com>
2017-09-27 14:31:06 +0530

    Update cheeta.py

--- pdfutils/cheeta.py
+++ pdfutils/cheeta.py
@@ -9,52 +9,98 @@ import datetime
 from bson import json_util
 from collections import defaultdict
 from httplib2 import Http
-import mongoengine
-
+from socialanalytics.utils.sentimentUtils import SentimentUtils
+from isodate import parse_duration
 DEVELOPER_KEY="AIzaSyBpWXegO6Vp9-73v8ifTA1kjwchGeXRj2E"
 YOUTUBE_API_SERVICE_NAME="youtube"
 YOUTUBE_API_VERSION="v3"
+sutils = SentimentUtils()
 
 youtube =  build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
-videosnippet = "contentDetails,id,liveStreamingDetails,localizations,player,recordingDetails,snippet,statistics,status,topicDetails"
-commentsnippet = "snippet,replies"
-channelsnippet = "auditDetails,brandingSettings,contentDetails,contentOwnerDetails,id,invideoPromotion,localizations,snippet,statistics,status,topicDetails"
 
+channelSnippet = "brandingSettings,contentDetails,contentOwnerDetails,id,invideoPromotion,localizations,snippet,statistics,status,topicDetails"
+videoSnippet = "contentDetails,id,liveStreamingDetails,localizations,player,recordingDetails,snippet,statistics,status,topicDetails"
+commentSnippet = "id,snippet"
 
 videos = []
 
 def comments(request, response, exception):
     if response is not None:
-        comments = []
-        print json.dumps(response)
+        commentsList = []
         for video in videos:
             if len(response['items']) > 0:
-                if video['videoid'] == response['items'][0].get('snippet', {}).get('videoId', 'NA'):
+                if video['videoId'] == response['items'][0].get('snippet', {}).get('videoId',None):
                     for item in response['items']:
-                        comments.append({'commentId':item['id']})
+                        snippet = item['snippet']
+                        snippet.__setitem__('channelId',video['channelId'])
+                        commentSnippet = snippet['topLevelComment']['snippet']
+                        commentText = commentSnippet['textOriginal']
+                        commentTextParsed = sutils.get_sentiment(commentText)
+                        commentSnippet['textOriginalParsed'] = commentTextParsed['text']
+                        snippet['topLevelComment']['snippet'] = commentSnippet
+                        snippet['topLevelComment']['snippet']['channelId'] = video['channelId']
 
+                        commentsList.append({'commentId':item['id'],
+                                             'snippet':snippet,
+                                             'polarity':commentTextParsed['polarity']})
+                    video['comments'] = commentsList
 
 def videoStats(request, response,exception):
     if response is not None:
-        print response
+        for video in videos:
+            if len(response.get('items', None)) > 0 and video['videoId'] == response['items'][0]['id']:
+                item = response['items'][0]
+                videoSnippet = item['snippet']
+                videoPolarity = sutils.get_sentiment(videoSnippet.get('description',None))
+                videoSnippet['descriptionParsed'] = videoPolarity.get('text')
+                video['snippet'] = videoSnippet
+                video['contentDetails'] = item.get('contentDetails',None)
+                video['contentDetails']['duration'] = int(parse_duration(item.get('contentDetails').get('duration')).total_seconds())
+                video['status'] = item.get('status',{})
+                video['statistics'] = item.get('statistics',{})
+                video['topicDetails'] = item.get('topicDetails',{})
+                video['liveStreamingDetails'] = item.get('liveStreamingDetails', {})
+                video['polarity'] = videoPolarity.get('polarity')
+
 
 def channelInfo(request,response,exception):
-
     if response is not None:
-        print response
+        for video in videos:
+            channelInfoDict = {}
+            if len(response.get('items', None)) > 0 and video['channelId'] == response['items'][0]['id']:
+                item = response['items'][0]
+                if item['id'] in channelInfoDict.keys():
+                    video['channel'] = channelInfoDict[item['id']]
+                else:
+                    channel = {'channelId':item['id'],'snippet':item['snippet'],
+                               'contentDetails':item.get('contentDetails',{}),
+                               'statistics':item.get('statistics',{}),
+                               'topicDetails':item.get('topicDetails',{}),
+                               'status':item.get('status',{}),
+                               'brandingSettings':item.get('brandingSettings',{}),
+                               'contentOwnerDetails':item.get('contentOwnerDetails',{})}
+                    channelInfoDict[item['id']] = channel
+                    video['channel'] = channel
 
-def getvideos(q, n=25):
-    search_response = youtube.search().list(q=q, part="id,snippet", maxResults=n, order='date',type='video').execute()
+    if exception is not None:
+        print exception
+
+def getvideos(q, n=10):
+    videos = []
+    search_response = youtube.search().list(q=q, part="id,snippet", maxResults=n, order='date',type='video',regionCode='IN').execute()
     batch = youtube.new_batch_http_request()
 
     for search_result in search_response:
+
         if 'items' in search_result:
             for item in search_response[search_result]:
-
-                videos.append({"videoid": item['id']['videoId'],"channelId":item['snippet']['channelId']})
-                batch.add(youtube.commentThreads().list(part="id,snippet,replies", videoId=item['id']['videoId'], maxResults=50,textFormat='plainText'), callback=comments)
-                #batch.add(youtube.videos().list(part="statistics,contentDetails,status,recordingDetails", id=item['id']['videoId'],maxResults=30), callback=videoStats)
-                #batch.add(youtube.channels().list(part="snippet,contentDetails,statistics,topicDetails,brandingSettings,status",id=item['snippet']['channelId'], maxResults=30), callback=channelInfo)
+                videos.append({"videoId": item['id']['videoId'],
+                               "title": item['snippet']['title'],
+                               "channelId": item['snippet']['channelId'],
+                               })
+                batch.add(youtube.commentThreads().list(part=commentSnippet, videoId=item['id']['videoId'], maxResults=50, textFormat='plainText'), callback=comments)
+                batch.add(youtube.videos().list(part=videoSnippet, id=item['id']['videoId'],maxResults=30), callback=videoStats)
+                batch.add(youtube.channels().list(part=channelSnippet,id=item['snippet']['channelId'], maxResults=30), callback=channelInfo)
     batch.execute()
 
-getvideos('narendramodi')
+    return videos
